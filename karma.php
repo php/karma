7c0e51a2399139c13ec4c7fab34fcc65caa67494
David Soria Parra <dsp@php.net>
2011-12-09 19:48:28 +0100

    Initial commit

---
+++ karma.php
@@ -0,0 +1,144 @@
+#!/usr/bin/env php
+<?php
+/*
+ * (c) 2011 David Soria Parra <dsp at php dot net>
+ *
+ * Licensed under the terms of the MIT license.
+ */
+
+namespace Karma;
+
+const KARMA_FILE = '/home/dsp/dev/php/SVNROOT/global_avail';
+const REPO_URL = '/home/dsp/dev/php/src/php-src.git';
+
+class GitReceiveHook {
+    const GIT_EXECUTABLE = 'git';
+    const INPUT_PATTERN = '@^([0-9a-f]{40}) SP ([0-9a-f]{40}) SP (\w+)$@i';
+
+    public function hookInput()
+    {
+        $parsed_input = [];
+        while (!feof(STDIN)) {
+            $line = fgets(STDIN);
+            if (preg_match(self::INPUT_PATTERN, $line, $matches)) {
+                $parsed_input[] = [
+                    'old'     => $matches[1],
+                    'new'     => $matches[2],
+                    'refname' => $matches[3]];
+            }
+        }
+        return $parsed_input;
+    }
+
+    private function getReceivedPathsForRange($old, $new)
+    {
+        $output = [];
+        exec(
+            sprintf('%s --git-dir=%s/.git log --name-only --pretty=format:"" %s..%s',
+                self::GIT_EXECUTABLE, REPO_URL, $old, $new), $output);
+        return $output;
+    }
+
+    public function getReceivedPaths()
+    {
+        $parsed_input = $this->hookInput();
+
+        $paths = array_map(
+            function ($input) {
+                return $this->getReceivedPathsForRange($input['old'], $input['new']);
+            },
+            $parsed_input);
+
+        /* remove empty lines, and flattern the array */
+        $flattend = array_reduce($paths, 'array_merge', []);
+        $paths    = array_filter($flattend);
+
+        return array_unique($paths);
+    }
+}
+
+
+function deny($reason)
+{
+    fwrite(STDERR, $reason . "\n");
+    exit(1);
+}
+
+function accept()
+{
+    exit(0);
+}
+
+function get_karma_for_paths($username, array $paths, \Iterator $avail_lines)
+{
+    $access = array_fill_keys($paths, 'unavail');
+    foreach ($avail_lines as $acl_line) {
+        $acl_line = trim($acl_line);
+        if ('' === $acl_line || '#' === $acl_line{0}) {
+            continue;
+        }
+
+        @list($avail, $user_str, $path_str) = explode('|', $acl_line);
+
+        $allowed_paths = explode(',', $path_str);
+        $allowed_users = explode(',', $user_str);
+
+        /* ignore lines which don't contain our users or apply to all users */
+        if (!in_array($username, $allowed_users) && !empty($user_str)) {
+            continue;
+        }
+
+        if (!in_array($avail, ['avail', 'unavail'])) {
+            continue;
+        }
+
+        if (empty($path_str)) {
+            $access = array_fill_keys($paths, $avail);
+        } else {
+            foreach ($access as $requested_path => $is_avail) {
+                foreach ($allowed_paths as $path) {
+                    if (fnmatch($path . '*', $requested_path)) {
+                        $access[$requested_path] = $avail;
+                    }
+                }
+            }
+        }
+    }
+
+    return $access;
+}
+
+function get_unavail_paths($username, array $paths, \Iterator $avail_lines)
+{
+    return
+        array_keys(
+            array_filter(
+                get_karma_for_paths($username, $paths, $avail_lines),
+                function ($avail) {
+                    return 'unavail' === $avail;
+                }));
+}
+
+
+error_reporting(E_ALL | E_STRICT);
+date_default_timezone_set('UTC');
+putenv("PATH=/usr/local/bin:/usr/bin:/bin");
+putenv("LC_ALL=en_US.UTF-8");
+
+$hook       = new GitReceiveHook();
+$karma_file = new \SplFileObject(KARMA_FILE);
+$karma_file->setFlags(\SplFileObject::DROP_NEW_LINE | \SplFileObject::SKIP_EMPTY);
+
+$requested_paths = $hook->getReceivedPaths();
+
+if (empty($requested_paths)) {
+    deny("We cannot figure out what you comitted!");
+}
+
+$unavail_paths = get_unavail_paths($_ENV['REMOTE_USER'], $requested_paths, $karma_file);
+
+if (!empty($unavail_paths)) {
+    deny("You are not allowed to write to " . implode(',', $unavail_paths));
+}
+
+accept();
