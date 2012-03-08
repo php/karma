2f637ccbfef5fcd60b2e905ea486b60100c020e2
Florian Anderiasch <fa@php.net>
2012-03-08 07:14:08 +0100

    Add methods to extract commit messages

--- lib/Git/ReceiveHook.php
+++ lib/Git/ReceiveHook.php
@@ -134,6 +134,52 @@ class ReceiveHook
         return $output;
     }
 
+    /**
+     * Returns an array of commit messages between revision $old and $new.
+     *
+     * @param string $old The old revison number.
+     * @parma string $new The new revison umber.
+     *
+     * @return array
+     */
+    private function getReceivedMessagesForRange($old, $new)
+    {
+        $repourl = $this->getRepositoryPath();
+        $output = [];
+
+        if ($old == '0000000000000000000000000000000000000000') {
+            $cmd = sprintf(
+                "%s --git-dir=%s for-each-ref --format='%%(refname)' 'refs/heads/*'",
+                self::GIT_EXECUTABLE,
+                $repourl
+            );
+            exec($cmd, $output);
+
+            /* do we have heads? otherwise it's a new repo! */
+            $heads = implode(' ', $output);
+            $not   = count($output) > 0 ? sprintf('--not %s', escapeshellarg($heads)) : '';
+            $cmd   = sprintf(
+                '%s --git-dir=%s log --pretty=format:"[%%ae] %%H %%s" %s %s',
+                self::GIT_EXECUTABLE,
+                $repourl,
+                $not,
+                escapeshellarg($new)
+            );
+            exec($cmd, $output);
+        } else {
+            $cmd = sprintf(
+                '%s --git-dir=%s log --pretty=format:"[%%ae] %%H %%s" %s..%s',
+                self::GIT_EXECUTABLE,
+                $repourl,
+                escapeshellarg($old),
+                escapeshellarg($new)
+            );
+            exec($cmd, $output);
+        }
+
+        return $output;
+    }
+
     public function getReceivedPaths()
     {
         $parsed_input = $this->hookInput();
@@ -141,6 +187,23 @@ class ReceiveHook
         $paths = array_map(
             function ($input) {
                 return $this->getReceivedPathsForRange($input['old'], $input['new']);
+            },
+            $parsed_input);
+
+        /* remove empty lines, and flattern the array */
+        $flattend = array_reduce($paths, 'array_merge', []);
+        $paths    = array_filter($flattend);
+
+        return array_unique($paths);
+    }
+
+    public function getReceivedMessages()
+    {
+        $parsed_input = $this->hookInput();
+
+        $paths = array_map(
+            function ($input) {
+                return $this->getReceivedMessagesForRange($input['old'], $input['new']);
             },
             $parsed_input);
 
